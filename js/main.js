d437ce4962ff3e5260e612c0fbb741fa09d7417d
hoooky <hoooky.ferreira74@gmail.com>
2024-08-26 15:33:35 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Função para atualizar a barra de progresso
+function updateProgress(barId, textId, value) {
+    const progressBar = document.getElementById(barId);
+    const progressText = document.getElementById(textId);
+    progressBar.style.width = value + '%';
+    progressText.textContent = value;
+}
+
+// Atualiza as barras de progresso
+updateProgress('progress1', 'progress-text-1', 100);
+updateProgress('progress2', 'progress-text-2', 100);
+updateProgress('progress3', 'progress-text-3', 100);
+updateProgress('progress4', 'progress-text-4', 70);
+updateProgress('progress5', 'progress-text-5', 25);
+updateProgress('progress6', 'progress-text-6', 50);
+updateProgress('progress6', 'progress-text-7', 50);
