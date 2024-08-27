af76351f785187ab49eae2206b522d0945adf3c1
hoooky <hoooky.ferreira74@gmail.com>
2024-08-27 09:19:47 -0300

    botoesnav
    funcionalidade dos botões principais

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ updateProgress('progress4', 'progress-text-4', 70);
 updateProgress('progress5', 'progress-text-5', 25);
 updateProgress('progress6', 'progress-text-6', 50);
 updateProgress('progress6', 'progress-text-7', 50);
+
+function scrollToSection(sobre) {
+    const section = document.getElementById(tecnologia);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToSection(tecnologia) {
+    const section = document.getElementById(tecnologia);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToSection(projetos) {
+    const section = document.getElementById(projetos);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
